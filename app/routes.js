2529b116aaafabd3c2e6852332b8af516f29ec72
mcoulthurst <mc@lightness.co.uk>
2020-03-16 14:02:44 +0000

    add first questions

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// landing page
+router.get("/submit", function (req, res, next) {
+    console.log("submit");
+    console.log(req.session.data);
+    
+    res.render("confirm", {
+      });
+});
+
+
 
 module.exports = router

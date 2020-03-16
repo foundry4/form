809e85b4daa18c9f4bea86bf57ed5ec548f77d21
mcoulthurst <mc@lightness.co.uk>
2020-03-16 14:51:12 +0000

    Add basic and speed parts of questionnaire

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,57 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+var categories = [
+    "Air Compressors / Pumps",
+    "Bellows",
+    "Self-inflating bags",
+    "Gas mixing valves",
+    "Pressure Regulators",
+    "Flow Control valves",
+    "Solenoid valves",
+    "Pressure relief valves",
+    "Check valves / one-way valves",
+    "Industrial Automation components (Safety Relays, PLCs)",
+    "Power Supplies ",
+    "Electric Motors, and motor controllers",
+    "Linear actuators and controllers",
+    "Tubing and fittings",
+    "Pressure Sensors and Indicators",
+    "Oxygen Sensors and Indicators",
+    "Flow Sensors and Indicators",
+    "Manometers",
+    "Heat and moisture exchanging filters (HMEFs)",
+    "Air Filter, HEPA Filters"
+];
+var expertise = [
+    "Design / specification",
+    "Rapid prototyping",
+    "Manufacturing (manual)",
+    "Manufacturing (automated)",
+    "Machine Shops/sheet metal/tool manufacture",
+    "Pneumatic part manufacturers/suppliers",
+    "Contract/Product Assembly",
+    "Certification/regulation/testing",
+    "Logistics",
+    "Medical Training"
+];
+
+var resources = [
+    "suitable space",
+    "equipment",
+    "trained personnel",
+    "other",
+];
+
+
 // landing page
+router.get("/", function (req, res, next) {
+    res.render("index", {
+        expertise: expertise,
+        resources: resources
+      });
+});
+
 router.get("/submit", function (req, res, next) {
     console.log("submit");
     console.log(req.session.data);

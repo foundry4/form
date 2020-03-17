7f213790d0e30e28cdf33ad6a8875a5651783fa0
mcoulthurst <mc@lightness.co.uk>
2020-03-17 09:00:03 +0000

    handle form data

--- app/routes.js
+++ app/routes.js
@@ -59,10 +59,160 @@ router.get("/", function (req, res, next) {
 
 router.get("/submit", function (req, res, next) {
     console.log("submit");
-    var json = JSON.stringify(req.session.data);
+    var data = req.session.data;
+    // pullout specific BASIC vars
+    var name = "";
+    if(data['organisation-name']){
+        name = data['organisation-name'];
+    }
+    var number = "";
+    if(data['company-number']){
+        number = data['company-number'];
+    }
+    var contact = "";
+    if(data['primary-contact']){
+        contact = data['primary-contact'];
+    }
+    var role = "";
+    if(data['primary-contact-role']){
+        role = data['primary-contact-role'];
+    }
+    var phone = "";
+    if(data['phone']){
+        phone = data['phone'];
+    }
+    var email = "";
+    if(data['email']){
+        email = data['email'];
+    }
+
+    console.log(name, number ,contact, phone, email);
+    // SUPPLY CHAIN
+
+    var isClinical = "no";
+    if(data['is-clinical']){
+        isClinical = data['is-clinical'];
+    }
+    var isHumanUse = "no";
+    if(data['human-use']){
+        isHumanUse = data['human-use'];
+    }
+    var isVetUse = "no";
+    if(data['vet-use']){
+        isVetUse = data['vet-use'];
+    }
+    var isOtherUse = "no";
+    if(data['other-use']){
+        isOtherUse = data['other-use'];
+    }
+
+    // freetext
+    var ventilatorText = "";
+    if(data['ventilator-detail']){
+        ventilatorText = data['ventilator-detail'];
+    }
+    console.log(isClinical, isHumanUse, isVetUse, isOtherUse, ventilatorText);
+
+    // MEDICAL DEVICES
+    var design = [];
+    if(data['design']){
+        design = data['design'];
+    }
+    var manufacture = [];
+    if(data['manufacture']){
+        manufacture = data['manufacture'];
+    }
+    var supply = [];
+    if(data['supply']){
+        supply = data['supply'];
+    }
+
+    
+    // get locations - build an array
+    var location = [];
+    for ( var i=1; i<21; i++){
+        var ref = 0;
+        if(data['location-'+i]!==""){
+            ref = parseInt(data['location-'+i]);
+        }
+        location.push(ref);
+    }
+    //console.log(design, manufacture, supply);
+    console.log(location);
+
+    // Q5
+    // freetext
+    var offerText = [];
+    if(data['offer']){
+        offerText = data['offer'];
+    }
+
+    // SKILLS and SPECIALISM
+    var skills = [];
+    if(data['skills']){
+        skills = data['skills'];
+    }
+    var specialism = [];
+    if(data['specialism']){
+        specialism = data['specialism'];
+    }
+    // get locations - build an array
+    var specialismLocation = [];
+    for ( var i=1; i<11; i++){
+        var ref = 0;
+        if(data['specialism-location-'+i]!==""){
+            ref = parseInt(data['specialism-location-'+i]);
+        }
+        specialismLocation.push(ref);
+    }
+console.log(specialismLocation);
+
+    // Q7
+    var resources = [];
+    if(data['resources']){
+        resources = data['resources'];
+    }
+    // freetext
+    var resourcesText = [];
+    if(data['resources-detail']){
+        resourcesText = data['resources-detail'];
+    }
+    console.log(resources, resourcesText);
+    
+    var SQL = `INSERT INTO companies(
+        name, number, contact, phone, email, 
+        isClinical, isHumanUse, isVetUse, isOtherUse, ventilatorText,
+        design, manufacture, supply, location,
+        offerText,
+        skills, specialism, specialismLocation, resources, resourcesText
+        ) VALUES (
+            '${name}', '${number}', '${contact}', '${phone}', '${email}', 
+            '${isClinical}', '${isHumanUse}', '${isVetUse}', '${isOtherUse}',' ${ventilatorText}',
+            '${design}', '${manufacture}', '${supply}',' ${location}',
+            '${offerText}',
+            '${skills}', '${specialism}', '${specialismLocation}', '${resources}', '${resourcesText}'
+        );`;
+
+    //var json = JSON.stringify(req.session.data);
+    //var SQL = "INSERT INTO companies(info) VALUES ('"+json+"');";
 
 
+    console.log(SQL);
+/*  
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+      });
+      
+    client.connect();
 
+    client.query(SQL, (err, res) => {
+        if (err) throw err;
+        
+        //console.log(res);
+        client.end();
+      });
+ */
     res.render("confirm", {
       });
 });

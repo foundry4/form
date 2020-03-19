02da173b589a7eaf4c71fba7f96025caa81b7d4d
Elizabeth Phillips <elizabethphillips@elizabeths-mbp.home>
2020-03-19 08:47:27 +0000

    tidy up and handle bad entries in DB

--- convertJSONScript.js
+++ convertJSONScript.js
@@ -1,25 +1,24 @@
 const { Client } = require('pg');
 const createData = require('./lib/createData');
-const parse = async (id,parsed)=> {
+
+const parse = async (id,parsed,client)=> {
   try {
     let SQL = '';
-    const { fieldNames, values } = createData(parsed);
-    for (let i = 0; i < fieldNames.length; i++) {
-      SQL = SQL + `UPDATE companies SET ${fieldNames[i]}='${values[i]}' WHERE id=${id};`;
-    }
-    const client = new Client({
-      connectionString: process.env.HEROKU_POSTGRESQL_RED_URL || process.env.DATABASE_URL,
-      ssl: true,
+    const { sql_values } = createData(JSON.parse(parsed));
+    Object.keys(sql_values).map((key)=> {
+      if (key === 'info'|| key.includes('unchecked') || key.includes('power_supplies _')|| key.includes('m_specialism')) {
+      }
+      else {
+        SQL = SQL + `UPDATE companies SET ${key}='${fix_quote(sql_values[key])}' WHERE id=${id} ;`;
+      }
     });
     await client.query(SQL);
-    client.end();
   }
   catch(err){
-    console.log(err.toString())
+    console.log('Error parsing JSON into columns;', err.toString());
   }
 }
 const getRows = async ()=>{
-  //
   try {
     const client = new Client({
       connectionString: process.env.HEROKU_POSTGRESQL_RED_URL || process.env.DATABASE_URL,
@@ -29,14 +28,22 @@ const getRows = async ()=>{
     client.connect();
     const SQL = 'SELECT * from companies;'
     let rows = await client.query(SQL);
-    console.log('Converting ',rows.rows.length,' rows');
-    await Promise.all(rows.rows.slice(1,2).map(async (row)=>{
-      parse(row.id,JSON.stringify(row.info),client)
+    console.log('Converting ',rows.rows.length,' rows',process.env.DATABASE_URL);
+    await Promise.all(rows.rows.map(async (row)=>{
+      await parse(row.id,JSON.stringify(row.info),client);
     }));
     client.end();
   }
   catch(err){
-    console.log(err.toString())
+    console.log('Error retrieving rows from database;', err.toString());
+  }
+}
+const fix_quote = (string) => {
+  if (typeof string === 'string'){
+    return string.replace("'" , "''");
+  }
+  else {
+    return string;
   }
 }
 getRows();
